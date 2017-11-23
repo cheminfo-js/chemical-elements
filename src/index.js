260c11cd37fb00ac048bf86f688a3bc5a5a5bc5f
Luc Patiny <luc@patiny.com>
2017-11-23 22:24:11 +0100

    add more information

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
 'use strict';
 
 var Wikidata=require('wikidata-sdk');
-var request = require('superagent');
+var fetch = require('node-fetch');
 
-function compileIsotopes() {
+async function compileIsotopes() {
 
     var CRLF='\r\n';
-    var url=Wikidata.sparqlQuery(
-        'PREFIX wd: <http://www.wikidata.org/entity/>'+CRLF+
-        'PREFIX wdt: <http://www.wikidata.org/prop/direct/>'+CRLF+
-        'SELECT ?isotope ?isotopeLabel ?atomicNumber ?halfLife ?halfLife_unitLabel ?decaysToLabel ?decaysModeLabel ?decaysProportion WHERE {'+CRLF+
-        'FILTER(?atomicNumber = 17) .'+CRLF+
-        '?isotopeOfElement wdt:P279 wd:Q25276 .'+CRLF+
-        '?isotopeK wdt:P279  ?isotopeOfElement .'+CRLF+
-        '?isotope wdt:P1086 ?atomicNumber .'+CRLF+
-
-        '?isotope p:P2114 ?halfLife_statement .'+CRLF+
-        '?halfLife_statement ps:P2114 ?halfLife .'+CRLF+
-        '?halfLife_statement psv:P2114/wikibase:quantityUnit ?halfLife_unit .'+CRLF+
-        'OPTIONAL { ?halfLife_statement pq:P817 ?halfLife_unit .}'+CRLF+
-
-        '?isotope p:P816 ?decaysTo_statement .'+CRLF+
-        '?decaysTo_statement ps:P816 ?decaysTo .'+CRLF+
-        'OPTIONAL { ?decaysTo_statement pq:P817 ?decaysMode .}'+CRLF+
-        'OPTIONAL { ?decaysTo_statement pq:P1107 ?decaysProportion .}'+CRLF+
-
-        'SERVICE wikibase:label {'+CRLF+
-        'bd:serviceParam wikibase:language "en" .'+CRLF+
-        '}'+CRLF+
-        '}'+CRLF+
-        'ORDER BY ASC(?atomicNumber)'+CRLF+
-        'LIMIT 10'+CRLF
+    var url=Wikidata.sparqlQuery(`
+    PREFIX wd: <http://www.wikidata.org/entity/>
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    
+    SELECT ?element ?symbol ?neutrons ?protons ?atomicNumber ?halfLife ?halfLife_unitLabel ?decaysToLabel ?decaysModel ?decaysProportion
+    WHERE
+    {
+      ?element wdt:P31 wd:Q11344;
+               wdt:P1086 ?protons;
+               wdt:P246 ?symbol.
+      ?isotope wdt:P279 ?element;
+               wdt:P1086 ?atomicNumber;
+               p:P2114 ?halfLife_statement;
+               p:P816 ?decaysTo_statement;
+               wdt:P1148 ?neutrons.
+      ?halfLife_statement ps:P2114 ?halfLife;
+                          psv:P2114/wikibase:quantityUnit ?halfLife_unit .
+      OPTIONAL { ?halfLife_statement pq:P817 ?halfLife_unit .}
+      
+    
+      ?decaysTo_statement ps:P816 ?decaysTo .
+      OPTIONAL { ?decaysTo_statement pq:P817 ?decaysMode .}
+      OPTIONAL { ?decaysTo_statement pq:P1107 ?decaysProportion .}
+      
+      SERVICE wikibase:label {
+         bd:serviceParam wikibase:language "en" .
+      }
+    }
+    ORDER BY ?protons
+    LIMIT 10
+       `
     );
 
-    request.get(url).then(
-        function(result) {
-            var data=JSON.parse(result.text).results.bindings;
-            
-            console.log(data);
-        }
-    ).catch(function(error) {
-        console.log(error);
-    })
+    var result = await fetch(url);
+    var data = await result.text();
+  //  console.log(result);
+
+
+    console.log(data);
 
 
 
 
 
-//http://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&ascii=ascii2&isotype=all
 
     return true;
 };
