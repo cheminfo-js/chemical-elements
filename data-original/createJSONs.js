1136f5b41ca602421f4b823b12b1be4d2fd8ab95
Luc Patiny <luc@patiny.com>
2017-11-28 22:57:55 +0100

    Add stableIsotopesObject

--- data-original/createJSONs.js
+++ data-original/createJSONs.js
@@ -27,10 +27,18 @@ fs.writeFileSync(__dirname + '/../src/elementsAndIsotopes.js', 'module.exports='
 
 var elementsAndStableIsotopes = JSON.parse(JSON.stringify(elementsAndIsotopes));
 // we remove all the unstable isotopes
+var stableIsotopesObject = {};
 elementsAndStableIsotopes.forEach((e) => {
     e.isotopes = e.isotopes.filter((i) => i.abundance > 0);
+    e.isotopes.forEach((i) => {
+        stableIsotopesObject[i.nominal + e.symbol] = {
+            abundance: i.abundance,
+            mass: i.mass
+        };
+    });
 });
 fs.writeFileSync(__dirname + '/../src/elementsAndStableIsotopes.js', 'module.exports=' + JSON.stringify(elementsAndStableIsotopes));
+fs.writeFileSync(__dirname + '/../src/stableIsotopesObject.js', 'module.exports=' + JSON.stringify(stableIsotopesObject));
 
 
 var elements = JSON.parse(JSON.stringify(elementsAndStableIsotopes));
@@ -44,23 +52,22 @@ fs.writeFileSync(__dirname + '/../src/elements.js', 'module.exports=' + JSON.str
 var elementsObject = {};
 elements.forEach((e) => {
     elementsObject[e.symbol] = e;
-    e.symbol=undefined;
+    e.symbol = undefined;
 });
 fs.writeFileSync(__dirname + '/../src/elementsObject.js', 'module.exports=' + JSON.stringify(elementsObject));
-
 
 
 var elementsAndIsotopesObject = {};
 elementsAndIsotopes.forEach((e) => {
     elementsAndIsotopesObject[e.symbol] = e;
-    e.symbol=undefined;
+    e.symbol = undefined;
 });
 fs.writeFileSync(__dirname + '/../src/elementsAndIsotopesObject.js', 'module.exports=' + JSON.stringify(elementsAndIsotopesObject));
 
 var elementsAndStableIsotopesObject = {};
 elementsAndStableIsotopes.forEach((e) => {
     elementsAndStableIsotopesObject[e.symbol] = e;
-    e.symbol=undefined;
+    e.symbol = undefined;
 });
 fs.writeFileSync(__dirname + '/../src/elementsAndStabeIsotopesObject.js', 'module.exports=' + JSON.stringify(elementsObject));
 
@@ -71,38 +78,38 @@ for (let group of groups) {
     let mf = group.mf;
     let mfObject = new MF(mf);
     let parts = mfObject.toParts()[0];
-    group.mass=0;
-    group.monoisotopicMass=0;
+    group.mass = 0;
+    group.monoisotopicMass = 0;
     group.elements = parts.map(part => {
         let number = part.multiplier;
-        let symbol = undefined;
-        switch(part.kind) {
+        let symbol;
+        switch (part.kind) {
             case Kind.ATOM: {
                 symbol = part.value;
                 let element = elementsObject[symbol];
-                if (! element) throw new Error('element unknown: '+symbol+' - '+part);
-                group.mass+=element.mass*number;
-                group.monoisotopicMass+=element.monoisotopicMass*number;
+                if (!element) throw new Error('element unknown: ' + symbol + ' - ' + part);
+                group.mass += element.mass * number;
+                group.monoisotopicMass += element.monoisotopicMass * number;
             }
-            break;
+                break;
             case Kind.ISOTOPE: {
-                symbol = '['+part.value.isotope+part.value.atom+']';
+                symbol = '[' + part.value.isotope + part.value.atom + ']';
                 let element = elementsAndIsotopesObject[part.value.atom];
-                if (! element) throw new Error('element unknown: '+part.value.atom+' - '+part);
-                let isotope = element.isotopes.filter( a => a.nominal===part.value.isotope)[0];
-                if (! isotope) throw new Error('isotope unknown: '+part.value.isotope+' - '+part);
-                group.mass+=isotope.mass*number;
-                group.monoisotopicMass+=isotope.mass*number;
-            }               
-            break;
+                if (!element) throw new Error('element unknown: ' + part.value.atom + ' - ' + part);
+                let isotope = element.isotopes.filter(a => a.nominal === part.value.isotope)[0];
+                if (!isotope) throw new Error('isotope unknown: ' + part.value.isotope + ' - ' + part);
+                group.mass += isotope.mass * number;
+                group.monoisotopicMass += isotope.mass * number;
+            }
+                break;
             default:
-            throw new Error('unknown type: '+part.kind);
+                throw new Error('unknown type: ' + part.kind);
         }
-        
+
         return {
             symbol,
             number
-        }
+        };
     });
 }
 
@@ -112,12 +119,9 @@ fs.writeFileSync(__dirname + '/../src/groups.js', 'module.exports=' + JSON.strin
 var groupsObject = {};
 groups.forEach((e) => {
     groupsObject[e.symbol] = e;
-    e.symbol=undefined;
+    e.symbol = undefined;
 });
 fs.writeFileSync(__dirname + '/../src/groupsObject.js', 'module.exports=' + JSON.stringify(groupsObject));
-
-
-
 
 
 function getMass(element) {
@@ -128,12 +132,12 @@ function getMass(element) {
 }
 
 function getMonoisotopicMass(element) {
-    var monoisotopicMass=undefined;
-    var maxAbundance=0;
+    var monoisotopicMass = undefined;
+    var maxAbundance = 0;
     for (let isotope of element.isotopes) {
-        if (isotope.abundance>maxAbundance) {
-            maxAbundance=isotope.abundance;
-            monoisotopicMass=isotope.mass;
+        if (isotope.abundance > maxAbundance) {
+            maxAbundance = isotope.abundance;
+            monoisotopicMass = isotope.mass;
         }
     }
     return monoisotopicMass;
